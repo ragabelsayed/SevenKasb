c79692d5d7dc595f74a962bbbe497feb1bc2ae44
ragab elsayed <ragabelsayed37@gmail.com>
2021-09-26 14:05:16 +0200

    add structure to unitScreen

--- sugarcane_juice_app/lib/screens/unit/unit_screen.dart
+++ sugarcane_juice_app/lib/screens/unit/unit_screen.dart
@@ -1,9 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sugarcane_juice_app/config/palette.dart';
+import 'package:sugarcane_juice_app/widget/menu_widget.dart';
 
-class UnitScreen extends StatelessWidget {
+class UnitScreen extends ConsumerWidget {
   static const routName = '/unit';
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+  Widget build(BuildContext context, ScopedReader watch) {
+    // final items = watch(itemProvider);
+    // items.fetchAndSetData();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Palette.primaryColor,
+        title: Text(
+          'وحدة القياس',
+          style: Theme.of(context)
+              .textTheme
+              .headline5!
+              .copyWith(color: Colors.white),
+        ),
+        centerTitle: true,
+        leading: MenuWidget(),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(20),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'اضافة وحدة قياس جديدة',
+        child: Icon(Icons.add),
+        backgroundColor: Colors.amber,
+        onPressed: () {
+          // showDialog(
+          //   context: context,
+          //   builder: (context) => ,
+          // );
+        },
+      ),
+    );
   }
 }
