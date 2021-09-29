dfa92a4d669bbab0b1ea94a0979e9cf5816e4724
ragab elsayed <ragabelsayed37@gmail.com>
2021-09-29 17:02:47 +0200

    active add newUnit btn in itemView

--- sugarcane_juice_app/lib/screens/main/widget/dropdown_unit_btn.dart
+++ sugarcane_juice_app/lib/screens/main/widget/dropdown_unit_btn.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:sugarcane_juice_app/config/constants.dart';
 import 'package:sugarcane_juice_app/config/palette.dart';
 import 'package:sugarcane_juice_app/models/unit.dart';
 import 'package:sugarcane_juice_app/providers/unit_provider.dart';
+import 'package:sugarcane_juice_app/screens/unit/widget/input_unit.dart';
 
 class DropdownUnitBtn extends ConsumerWidget {
-  Function(int newValue) unitId;
+  final Function(int newValue) unitId;
   DropdownUnitBtn({required this.unitId});
 
   @override
@@ -21,42 +20,66 @@ class DropdownUnitBtn extends ConsumerWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        IconButton(
-          icon: Icon(Icons.add),
-          onPressed: () {},
-          color: Colors.amber,
-        ),
-        SizedBox(
-          // width: MediaQuery.of(context).size.width * 0.5,
-          child: DropdownButton<String>(
-            value: _dropdownValue,
-            style: const TextStyle(
-              color: Colors.black,
-            ),
-            underline: Container(
-              height: 1,
-              color: Colors.amberAccent,
-            ),
-            alignment: Alignment.center,
-            hint: Text('وحدة القياس'),
-            isDense: true,
-            onChanged: (String? newValue) {
-              context.read(unitProvider).setCurrentUnit(newValue!);
-              if (newValue.isNotEmpty) {
-                int id =
-                    unitList.firstWhere((unit) => unit.name == newValue).id!;
-                unitId(id);
-              }
-            },
-            items: unitMenu
-                .map(
-                  (unit) => DropdownMenuItem(
-                    value: unit,
-                    child: Text(unit),
-                  ),
-                )
-                .toList(),
+        Container(
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, 6),
+                blurRadius: 10,
+                color: Color(0xFFB0B0B0).withOpacity(0.2),
+              ),
+            ],
           ),
+          child: OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              padding: EdgeInsets.zero,
+              backgroundColor: Palette.primaryLightColor,
+              primary: Colors.amber,
+              side: BorderSide.none,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50),
+              ),
+            ),
+            child: Icon(
+              Icons.add,
+              size: 27,
+            ),
+            onPressed: () => showDialog(
+              context: context,
+              builder: (context) => InputUnit(),
+            ),
+          ),
+        ),
+        DropdownButton<String>(
+          value: _dropdownValue,
+          style: const TextStyle(
+            color: Colors.black,
+          ),
+          underline: Container(
+            height: 1,
+            color: Colors.amberAccent,
+          ),
+          alignment: Alignment.center,
+          hint: Text('وحدة القياس'),
+          isDense: true,
+          onChanged: (String? newValue) {
+            context.read(unitProvider).setCurrentUnit(newValue!);
+            if (newValue.isNotEmpty) {
+              int id = unitList.firstWhere((unit) => unit.name == newValue).id!;
+              unitId(id);
+            }
+          },
+          items: unitMenu
+              .map(
+                (unit) => DropdownMenuItem(
+                  value: unit,
+                  child: Text(unit),
+                ),
+              )
+              .toList(),
         ),
       ],
     );
