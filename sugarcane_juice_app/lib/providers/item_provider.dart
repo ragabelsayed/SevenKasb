13e9b28f827c03efb27bb6dfc3c1722142f5eff5
ragab elsayed <ragabelsayed37@gmail.com>
2021-10-17 19:13:06 +0200

    add http excpetion file

--- sugarcane_juice_app/lib/providers/item_provider.dart
+++ sugarcane_juice_app/lib/providers/item_provider.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
+import 'package:sugarcane_juice_app/models/http_exception.dart';
 import 'package:sugarcane_juice_app/models/item.dart';
 import 'package:sugarcane_juice_app/models/unit.dart';
 import 'package:sugarcane_juice_app/providers/auth.dart';
