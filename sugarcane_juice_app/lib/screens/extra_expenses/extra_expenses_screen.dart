e8ea06f6ae1cdb29d19e624ba6a3eff261bff6ab
ragab elsayed <ragabelsayed37@gmail.com>
2021-10-29 20:38:26 +0200

    add extra dataTable view file

--- sugarcane_juice_app/lib/screens/extra_expenses/extra_expenses_screen.dart
+++ sugarcane_juice_app/lib/screens/extra_expenses/extra_expenses_screen.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:sugarcane_juice_app/screens/extra_expenses/widget/input_extra_expenses_form.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sugarcane_juice_app/providers/extra_provider.dart';
+import 'package:sugarcane_juice_app/screens/extra_expenses/widget/extra_data_table_view.dart';
+import 'package:sugarcane_juice_app/widget/error_view.dart';
 import '/config/constants.dart';
 import '/config/palette.dart';
 import '/widget/menu_widget.dart';
+import 'widget/input_extra_expenses_form.dart';
 
-class ExtraExpensesScreen extends StatelessWidget {
+class ExtraExpensesScreen extends ConsumerWidget {
   static const routName = '/extra_expenses';
   const ExtraExpensesScreen({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader watch) {
+    final extraExpenses = watch(extraExpensesProvider);
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -20,7 +25,25 @@ class ExtraExpensesScreen extends StatelessWidget {
         leading: MenuWidget(),
         shape: AppConstants.appBarBorder,
       ),
-      body: SizedBox(),
+      body: extraExpenses.when(
+        loading: () => Center(
+          child: const CircularProgressIndicator(color: Colors.green),
+        ),
+        error: (error, stackTrace) => ErrorView(error: error.toString()),
+        data: (extraList) {
+          var date = DateTime.now();
+          var thirtyDaysAgo = date.subtract(const Duration(days: 30));
+          var extras = extraList
+              .where((extra) => extra.dataTime.isAfter(thirtyDaysAgo))
+              .toList();
+          // return extras.isNotEmpty
+          return extraList.isNotEmpty
+              ? ExtraDataTableView(extraList: extraList)
+              : const Center(
+                  child: ErrorView(error: 'لا يوجد مصروفات إضافية'),
+                );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         tooltip: 'اضافة مصاريف إضافية',
         child: const Icon(Icons.add),
---
+++ sugarcane_juice_app/lib/screens/extra_expenses/widget/extra_data_table_view.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart' as intl;
+
+import 'package:sugarcane_juice_app/models/extra_expenses.dart';
+
+class ExtraDataTableView extends StatelessWidget {
+  final List<Extra> extraList;
+  const ExtraDataTableView({required this.extraList});
+
+  @override
+  Widget build(BuildContext context) {
+    final columns = [
+      'عرض',
+      'التاريخ',
+      'المصروف',
+      'الصارف',
+    ];
+    return SingleChildScrollView(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: DataTable(
+        columns: _getColumns(columns),
+        rows: _getRow(extras: extraList, context: context),
+        headingTextStyle:
+            Theme.of(context).textTheme.headline6!.copyWith(fontSize: 18),
+        columnSpacing: MediaQuery.of(context).size.width / 8,
+        horizontalMargin: 0.0,
+        showBottomBorder: true,
+      ),
+    );
+  }
+
+  List<DataColumn> _getColumns(List<String> columns) => columns
+      .map(
+        (column) => DataColumn(
+          numeric: true,
+          label: SizedBox(
+            width: 56,
+            child: Text(
+              column,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      )
+      .toList();
+
+  List<DataRow> _getRow(
+          {required List<Extra> extras, required BuildContext context}) =>
+      extras.map(
+        (extra) {
+          final cells = [
+            extra.id,
+            extra.cash,
+            intl.DateFormat.Md().format(extra.dataTime),
+          ];
+          return DataRow(
+            cells: [
+              ..._getCells(cells),
+              DataCell(
+                TextButton(
+                  child: Transform.rotate(
+                    angle: 600,
+                    origin: Offset(0.0, 0.0),
+                    child: Icon(
+                      Icons.forward_rounded,
+                      size: 30,
+                      color: Colors.amber,
+                    ),
+                  ),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return SizedBox();
+                      },
+                    );
+                  },
+                ),
+              ),
+            ].reversed.toList(),
+          );
+        },
+      ).toList();
+
+  List<DataCell> _getCells(List<dynamic> cells) {
+    var cellList = cells
+        .map(
+          (cell) => DataCell(
+            SizedBox(
+              width: 56,
+              child: Text(
+                '$cell',
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            // placeholder: true,
+          ),
+        )
+        .toList();
+    return cellList;
+  }
+}
