682c86af906a44bba558ff40b030a3972d54fd71
ragab elsayed <ragabelsayed37@gmail.com>
2021-11-05 18:58:28 +0200

    home screen is finished

--- sugarcane_juice_app/lib/screens/home_screen.dart
+++ sugarcane_juice_app/lib/screens/home_screen.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
-import 'package:sugarcane_juice_app/config/routes.dart';
-import 'package:sugarcane_juice_app/screens/bill/bill_screen.dart';
-import 'package:sugarcane_juice_app/screens/extra_expenses/extra_expenses_screen.dart';
-import 'package:sugarcane_juice_app/screens/item/item_screen.dart';
-import 'package:sugarcane_juice_app/screens/menu/menu_screen.dart';
-import 'package:sugarcane_juice_app/screens/offline/offline_screen.dart';
-import 'package:sugarcane_juice_app/screens/unit/unit_screen.dart';
-import 'package:sugarcane_juice_app/screens/user/user_screen.dart';
-
-import 'item/item_screen.dart';
+import '/config/routes.dart';
+import 'menu/menu_screen.dart';
 
 class HomeScreen extends ConsumerWidget {
   static const routName = '/home';
@@ -24,15 +16,19 @@ class HomeScreen extends ConsumerWidget {
       borderRadius: 30,
       slideWidth: MediaQuery.of(context).size.width * 0.7,
       showShadow: true,
+      openCurve: Curves.fastOutSlowIn,
+      closeCurve: Curves.fastOutSlowIn,
       backgroundColor: Colors.amberAccent,
       // isRtl: true,
       mainScreen: itemNotifier.getScreen(),
-      menuScreen: MenuScreen(
-        currentItem: currentItem,
-        onSelectedItem: (item) {
-          context.read(menuItemProvider.notifier).setItem(item);
-          // ZoomDrawer.of(context)!.close();
-        },
+      menuScreen: Builder(
+        builder: (context) => MenuScreen(
+          currentItem: currentItem,
+          onSelectedItem: (item) {
+            context.read(menuItemProvider.notifier).setItem(item);
+            ZoomDrawer.of(context)!.close();
+          },
+        ),
       ),
     );
   }
