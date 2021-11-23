4524b4b07e3c40357b9deee37dfb41dcaa012c99
ragab elsayed <ragabelsayed37@gmail.com>
2021-11-23 14:34:29 +0200

    add screen taps file to inventory screen

--- sugarcane_juice_app/lib/screens/inventory/inventory_screen.dart
+++ sugarcane_juice_app/lib/screens/inventory/inventory_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 import '/config/constants.dart';
 import '/config/palette.dart';
 import '/widget/menu_widget.dart';
+import 'widget/purchase_inventory.dart';
+import 'widget/sell_inventory.dart';
 
 class InventoryScreen extends StatelessWidget {
   const InventoryScreen({Key? key}) : super(key: key);
@@ -10,16 +14,39 @@ class InventoryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     FToast fToast = FToast().init(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'الجرد',
-          style: AppConstants.appBarTitle,
+    return DefaultTabController(
+      length: 2,
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('الجرد', style: AppConstants.appBarTitle),
+            centerTitle: true,
+            backgroundColor: Palette.primaryColor,
+            leading: const MenuWidget(),
+            shape: AppConstants.appBarBorder,
+            bottom: const TabBar(
+              indicatorPadding: EdgeInsets.symmetric(horizontal: 20),
+              indicatorColor: Colors.amber,
+              tabs: [
+                Tab(
+                  text: 'مبيعات',
+                  icon: FaIcon(FontAwesomeIcons.clipboardList),
+                ),
+                Tab(
+                  text: 'مشتريات',
+                  icon: FaIcon(FontAwesomeIcons.clipboardList),
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              PurchaseInventory(fToast: fToast),
+              SellInventory(fToast: fToast),
+            ],
+          ),
         ),
-        centerTitle: true,
-        backgroundColor: Palette.primaryColor,
-        leading: const MenuWidget(),
-        shape: AppConstants.appBarBorder,
       ),
     );
   }
---
+++ sugarcane_juice_app/lib/screens/inventory/widget/purchase_inventory.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class PurchaseInventory extends StatelessWidget {
+  final FToast fToast;
+  const PurchaseInventory({Key? key, required this.fToast}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
